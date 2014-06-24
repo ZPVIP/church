dfd980fd6adf5369163af00502d542e84129f490
PeterZhang <zpregister@gmail.com>
2014-06-24 22:33:28 +0200

    change default_url_options to ENV[APP_DOMAIN]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Caachen::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'caachen.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV["APP_DOMAIN"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
